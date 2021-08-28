5274eec82860d30da08c5b9d6a39b09cfae117a3
james <7298563+lamesjinden@users.noreply.github.com>
2021-08-27 23:31:10 -0400

    Base requested window dimensions on extents-type - for XFCE.

--- src/wpad/bottom.clj
+++ src/wpad/bottom.clj
@@ -1,40 +1,48 @@
 (ns wpad.bottom
   (:require [wpad.core :as w]))
 
-(def sizing-ratios [0.25 0.33 0.5 0.67 0.85 1.0])
+(def sizing-ratios [0.25 0.5 0.75 1.0])
 
-; todo use screen-x-offset
-
-(defn get-placements-by-rate [ratio containing-screen workspace-dimensions]
-  (let [screen-width (:width containing-screen)
-        screen-height (:height containing-screen)
-        workspace-height (:height workspace-dimensions)
-        y-delta (- screen-height workspace-height)
-        width (int (* screen-width ratio))
-        height (int (/ workspace-height 2))
+(defn get-placements-by-rate
+  [ratio
+   {screen-width :width
+    :as          _containing-screen}
+   {{workspace-height :height
+     :as              _workspace-dimensions}   :workspace
+    {{:keys [left-extent right-extent top-extent bottom-extent extents-type]
+      :as   _frame-dimensions} :frame-extents} :window
+    :as                                        _environment}]
+  (let [width (if (= extents-type :gtk)
+                (+ left-extent right-extent (int (* screen-width ratio)))
+                (int (* screen-width ratio)))
+        height (if (= extents-type :gtk)
+                 (+ top-extent bottom-extent (int (/ workspace-height 2)))
+                 (- (int (/ workspace-height 2)) top-extent bottom-extent))
         x (int (/ (- screen-width width) 2))
-        y (+ height y-delta)]
+        y (if (= extents-type :gtk)
+            (int (/ workspace-height 2))
+            (- (+ top-extent (int (/ workspace-height 2))) bottom-extent))]
     {:x      x
      :y      y
      :width  width
      :height height}))
 
-(defn get-placement-options [active-window-dimensions screens-dimensions workspace-dimensions]
+(defn get-placement-options
+  [{{active-window-dimensions :dimensions} :window
+    screens-dimensions                     :monitors
+    :as                                    environment}]
   (let [containing-screen (w/get-containing-screen active-window-dimensions screens-dimensions)]
-    (map
-      #(get-placements-by-rate % containing-screen workspace-dimensions)
-      sizing-ratios)))
+    (->> sizing-ratios
+         (map #(get-placements-by-rate % containing-screen environment)))))
 
 (defn -main []
-  (let [active-window-dimensions (w/get-active-window-dimensions)
-        frame-dimensions (w/get-frame-dimensions)
-        screens-dimensions (w/get-screens-dims)
-        workspace-dimensions (w/get-workspace-area)
-        placement-options (get-placement-options
-                            active-window-dimensions
-                            screens-dimensions
-                            workspace-dimensions)]
-    (w/move-next! active-window-dimensions frame-dimensions placement-options)))
+  (w/log "")
+  (try
+    (let [environment (w/get-environment)
+          placement-options (get-placement-options environment)]
+      (w/move-next! environment placement-options))
+    (catch Exception e
+      (w/log e))))
 
 (comment
 
--- src/wpad/core.clj
+++ src/wpad/core.clj
@@ -2,8 +2,13 @@
   (:require [clojure.java.shell :refer [sh]]
             [clojure.string :as str]))
 
-;(def xprop-root! (memoize (fn [] (sh "xprop" "-root"))))
-(def xprop-root! (fn [] (sh "xprop" "-root")))
+(def logfile-path "/home/james/bin/wpad/log.txt")
+
+(defn log [s]
+  ;(spit logfile-path (str s "\n") :append true)
+  )
+
+(def get-xprop-root (fn [] (sh "xprop" "-root")))
 
 (def xprop-active-window-pattern #"_NET_ACTIVE_WINDOW\(WINDOW\): window id # 0x([a-f0-9]+).*")
 
@@ -21,33 +26,27 @@
                               (parse-xprop-active-window-line))]
     active-window-id))
 
-(def wmctrl-lG-pattern #"(0x[a-f0-9]+)\s+\d+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+).*")
+(def xwininfo-upper-left-x-token "Absolute upper-left X")
+(def xwininfo-upper-left-y-token "Absolute upper-left Y")
+(def xwininfo-width-token "Width")
+(def xwininfo-height-token "Height")
 
-(defn parse-wmctrl-window-dimensions-line [s]
-  (when-let [match (re-matches wmctrl-lG-pattern s)]
-    (let [x (Integer/parseInt (get match 2))
-          y (Integer/parseInt (get match 3))
-          width (Integer/parseInt (get match 4))
-          height (Integer/parseInt (get match 5))]
-      {:x      x
-       :y      y
-       :width  width
-       :height height})))
-
-(defn parse-wmctrl-window-dimensions [ss active-window-id]
-  (let [active-window-dimensions (->> ss
-                                      (str/split-lines)
-                                      (filter (fn [s] (.startsWith (.toLowerCase s) (.toLowerCase active-window-id))))
-                                      (first)
-                                      (parse-wmctrl-window-dimensions-line))]
-    (into {:window-id active-window-id} active-window-dimensions)))
-
-(defn get-active-window-dimensions []
-  (let [xprop-root-out (:out (xprop-root!))
-        wmctrl-out (:out (sh "wmctrl" "-lG"))
-        active-window-id (parse-xprop-active-window-id xprop-root-out)
-        active-window-dimensions (parse-wmctrl-window-dimensions wmctrl-out active-window-id)]
-    active-window-dimensions))
+(defn parse-xwininfo-window-dimensions [ss]
+  (->> ss
+       (str/split-lines)
+       (map #(str/split % #":"))
+       (filter #(= 2 (count %)))
+       (reduce
+         (fn [acc [left right]]
+           (let [trim-left (str/trim left)
+                 trim-right (str/trim right)]
+             (cond
+               (= trim-left xwininfo-upper-left-x-token) (assoc acc :x (Integer/parseInt trim-right))
+               (= trim-left xwininfo-upper-left-y-token) (assoc acc :y (Integer/parseInt trim-right))
+               (= trim-left xwininfo-width-token) (assoc acc :width (Integer/parseInt trim-right))
+               (= trim-left xwininfo-height-token) (assoc acc :height (Integer/parseInt trim-right))
+               :else acc)))
+         {})))
 
 (def xrandr-monitors-line-pattern #"^Monitors:\s+(\d+).*")
 
@@ -82,11 +81,6 @@
     {:monitors-count       monitors-count
      :monitor-descriptions monitor-descriptions}))
 
-(defn get-screens-dims []
-  (let [xrandr-out (:out (sh "xrandr" "--listmonitors"))
-        parsed (parse-xrandr xrandr-out)]
-    parsed))
-
 (def xprop-workspace-pattern #"_NET_WORKAREA\(CARDINAL\)\s+=\s+(\d+), (\d+), (\d+), (\d+),?.*")
 
 (defn parse-xprop-workarea-line [s]
@@ -108,129 +102,178 @@
                             (parse-xprop-workarea-line))]
     workspace-area))
 
-(defn get-workspace-area []
-  (let [xprop-out (:out (sh "xprop" "-root"))
-        workspace-area (parse-xprop-workspace-area xprop-out)]
-    workspace-area))
-
-(def xprop-frame-extents-pattern #"_NET_FRAME_EXTENTS\(CARDINAL\) = (\d+), (\d+), (\d+), (\d+)")
+;_GTK_FRAME_EXTENTS(CARDINAL) = 26, 26, 23, 29
+;_NET_FRAME_EXTENTS(CARDINAL) = 0, 0, 37, 0
+(def xprop-frame-extents-pattern #".*_FRAME_EXTENTS\(CARDINAL\) = (\d+), (\d+), (\d+), (\d+)")
 
 (defn parse-xprop-frame-extents-line [s]
-  (when-let [match (re-matches xprop-frame-extents-pattern s)]
-    (let [left-extent (Integer/parseInt (get match 1))
-          right-extent (Integer/parseInt (get match 2))
-          top-extent (Integer/parseInt (get match 3))
-          bottom-extent (Integer/parseInt (get match 4))]
-      {:left-extent   left-extent
-       :right-extent  right-extent
-       :top-extent    top-extent
-       :bottom-extent bottom-extent})))
+  (when s
+    (when-let [match (re-matches xprop-frame-extents-pattern s)]
+      (let [left-extent (Integer/parseInt (get match 1))
+            right-extent (Integer/parseInt (get match 2))
+            top-extent (Integer/parseInt (get match 3))
+            bottom-extent (Integer/parseInt (get match 4))]
+        {:left-extent   left-extent
+         :right-extent  right-extent
+         :top-extent    top-extent
+         :bottom-extent bottom-extent}))))
+
+(def frame-extents-gtk-prefix "_GTK_FRAME_EXTENTS(CARDINAL)")
+(def frame-extents-net-prefix "_NET_FRAME_EXTENTS(CARDINAL)")
 
 (defn parse-xprop-frame-extents [ss]
-  (let [frame-extents (->> ss
-                           (str/split-lines)
-                           (filter (fn [s] (.startsWith s "_NET_FRAME_EXTENTS(CARDINAL)")))
-                           (first)
-                           (parse-xprop-frame-extents-line))]
-    frame-extents))
-
-(defn get-frame-dimensions []
-  (let [xprop-root-out (:out (sh "xprop" "-root"))
-        active-window-id (parse-xprop-active-window-id xprop-root-out)
-        xprop-id-out (:out (sh "xprop" "-id" active-window-id))
-        frame-extents (parse-xprop-frame-extents xprop-id-out)]
-    frame-extents))
+  (let [frame-extents-lines (->> ss
+                                 (str/split-lines)
+                                 (filter #(or (.startsWith % frame-extents-gtk-prefix)
+                                              (.startsWith % frame-extents-net-prefix))))
+        frame-extents-line (or (some #(and (.startsWith % frame-extents-gtk-prefix) %) frame-extents-lines)
+                               (some #(and (.startsWith % frame-extents-net-prefix) %) frame-extents-lines))
+        frame-extents (or (parse-xprop-frame-extents-line frame-extents-line)
+                          {:left-extent   0
+                           :right-extent  0
+                           :top-extent    0
+                           :bottom-extent 0})
+        extents-type (cond
+                       (nil? frame-extents-line) :none
+                       (.startsWith frame-extents-line frame-extents-gtk-prefix) :gtk
+                       (.startsWith frame-extents-line frame-extents-net-prefix) :net
+                       :else :none)]
+    (assoc frame-extents :extents-type extents-type)))
 
 (defn restore-active-window! []
   (as-> "wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz" $
         (str/split $ #" ")
         (apply sh $)))
 
-(defn -adjust-dimensions [{:keys [x y width height] :as _window-dimensions}
-                          {:keys [left-extent right-extent top-extent bottom-extent] :as _frame-dimensions}]
-  {:x      x
-   :y      y
-   :width  (- width left-extent right-extent)
-   :height (- height top-extent bottom-extent)})
-
-(defn move-active-window! [{:keys [x y] :as window-dimensions} frame-extents]
+(defn move-active-window! [{:keys [x y width height] :as _window-dimensions} _frame-extents]
   ; remove maximized flags, otherwise resizing has no effect
   (restore-active-window!)
-  (let [{adjusted-width  :width
-         adjusted-height :height} (-adjust-dimensions window-dimensions frame-extents)]
-    (as-> (format "wmctrl -r :ACTIVE: -e 0,%s,%s,%s,%s" x y adjusted-width adjusted-height) $
-          (str/split $ #" ")
-          (apply sh $))))
+  (log (format "desired dimensions: x=%s; y=%s; w=%s; h=%s" x y width height))
+  (as-> (format "wmctrl -r :ACTIVE: -e 0,%s,%s,%s,%s" x y width height) $
+        (str/split $ #" ")
+        (apply sh $)))
 
-(defn get-containing-screen [{window-x :x}
+(defn get-containing-screen [{_window-x :x}
                              {monitor-descriptions :monitor-descriptions}]
   ; find which screen the current window's x coordinate lies within.
   ; use found screen to provide screen-width.
   ; assume that iteration order of screens is correct.
   ;   i.e. screen 0 begins at x = 0, screen 1 begins at x = 1 + <WIDTH-OF-SCREEN-0>
-  (->> monitor-descriptions
-       (reduce (fn [acc {width :width :as monitor}]
-                 ; special case - when window-x is negative
-                 (if (and (zero? acc)
-                          (neg? window-x))
-                   (reduced (assoc monitor :x-offset 0))
-                   (if (<= acc window-x (+ acc width))
-                     (reduced (assoc monitor :x-offset acc))
-                     (+ acc width))))
-               0)))
+  (-> monitor-descriptions
+      (first)
+      (assoc :x-offset 0)))
 
-(defn -dimensions-match?
-  "determines if the two provided dimension descriptions are equal-ish.
+(defn parse-normal-hint-value [hint-value]
+  ; examples:
+  ;   program specified minimum size: 387 by 145
+  ;   program specified resize increment: 6 by 13
+  ;   program specified base size: 66 by 101
 
-   a match is defined as follows:
-   * when width is equal
-   * and
-   * when delta x is within twice the frame's x size
+  (let [split (str/split hint-value #" by ")
+        width (Integer/parseInt (str/trim (first split)))
+        height (Integer/parseInt (str/trim (second split)))]
+    {:hint-width  width
+     :hint-height height}))
 
-   for background, see https://askubuntu.com/questions/576604/what-causes-the-deviation-in-the-wmctrl-window-move-command
-   * though, under xfce 4.12 and through x2go client, not everything mentioned applied
-   * xdotool getactivewindow getwindoegeometry == xdotool getwindowgeometry $(xdotool selectwindow <click>)
-   "
-  [window-dimensions1 window-dimensions2 frame-dimensions]
-  (let [{width1 :width x1 :x} window-dimensions1
-        {width2 :width x2 :x} window-dimensions2
-        {:keys [left-extent right-extent]} frame-dimensions]
-    (and (= width1 width2)
-         (<= (Math/abs ^Integer (- x1 x2))
-             (* 2 (+ left-extent right-extent))))))
+(def hint-minimum-size-token "program specified minimum size")
+(def hint-resize-increment-token "program specified resize increment")
+(def hint-base-size-token "program specified base size")
 
-(defn move-next! [active-window-dimensions frame-dimensions centering-options]
-  (let [nearest (->> centering-options
+(defn parse-xprop-normal-hints [ss]
+  (->> ss
+       (str/split-lines)
+       (map #(str/split % #":"))
+       (filter #(= 2 (count %)))
+       (reduce
+         (fn [acc [left right]]
+           (let [trim-left (str/trim left)
+                 trim-right (str/trim right)]
+             (cond
+               (= trim-left hint-minimum-size-token) (assoc acc :minimum-size (parse-normal-hint-value trim-right))
+               (= trim-left hint-resize-increment-token) (assoc acc :resize-increment (parse-normal-hint-value trim-right))
+               (= trim-left hint-base-size-token) (assoc acc :width (parse-normal-hint-value trim-right))
+               :else acc)))
+         {})))
+
+(defn get-environment []
+  (let [xprop-root-out (:out (get-xprop-root))
+        active-window-id (parse-xprop-active-window-id xprop-root-out)
+        xwininfo-out (:out (sh "xwininfo" "-id" active-window-id))
+        active-window-dimensions (parse-xwininfo-window-dimensions xwininfo-out)
+        xprop-id-out (:out (sh "xprop" "-id" active-window-id))
+        frame-extents (parse-xprop-frame-extents xprop-id-out)
+        xrandr-out (:out (sh "xrandr" "--listmonitors"))
+        monitors (parse-xrandr xrandr-out)
+        workspace-area (parse-xprop-workspace-area xprop-root-out)
+        normal-hints (parse-xprop-normal-hints xprop-id-out)]
+    {:window    {:id            active-window-id
+                 :dimensions    active-window-dimensions
+                 :frame-extents frame-extents
+                 :hints         normal-hints}
+     :workspace workspace-area
+     :monitors  monitors}))
+
+(defn resize-dimensions [{width  :width
+                          height :height
+                          :as    dimensions}
+                         {:keys [hint-width hint-height] :as _resize-increments}
+                         {left-extent  :left-extent
+                          right-extent :right-extent
+                          :as          _frame-dimensions}
+                         {workspace-width :width
+                          :as             _workspace-dimensions}]
+  (let [total-width (+ workspace-width left-extent right-extent)
+        next-width (as-> (+ (- width (mod width hint-width)) hint-width) $
+                         ; resize down if adjusted size exceeds screen dimensions
+                         (if (> $ total-width)
+                           (- $ hint-width)
+                           $))
+        next-height (+ (- height (mod height hint-height)) hint-height)
+        next-dimensions (-> dimensions
+                            (assoc :width next-width)
+                            (assoc :height next-height))]
+    next-dimensions))
+
+(defn move-next!
+  [{{{active-window-width :width
+      :as                 _active-window-dimensions} :dimensions
+     frame-dimensions                                :frame-extents
+     {resize-increments :resize-increment
+      :as               _active-window-hints}        :hints} :window
+    workspace-dimensions                                     :workspace
+    :as                                                      _environment}
+   placement-options]
+  (let [resized-placements (if resize-increments
+                             (->> placement-options
+                                  (map #(resize-dimensions % resize-increments frame-dimensions workspace-dimensions)))
+                             placement-options)
+        nearest (->> resized-placements
                      (partition 2 1)
-                     (some (fn [[option next-option]]
-                             (and (-dimensions-match?
-                                    active-window-dimensions
-                                    (-adjust-dimensions option frame-dimensions)
-                                    frame-dimensions)
-                                  next-option))))
-        next-dimensions (or nearest (first centering-options))]
+                     (some (fn [[{option-width :width :as _option}
+                                 {next-width :width :as next-option}]]
+                             (and
+                               (>= active-window-width option-width)
+                               (< active-window-width next-width)
+                               next-option))))
+        next-dimensions (or nearest (first resized-placements))]
     (move-active-window! next-dimensions frame-dimensions)))
 
 (comment
   ; required binaries:
   ;   * xprop
   ;     * provides dimensions of the virtual workspace - i.e. top panel height
-  ;     * provides dimensions of active windoe frame extents i.e. thickness of title bar
+  ;     * provides dimensions of active window frame extents i.e. thickness of title bar
   ;   * xrandr
   ;     * provides physical screen dimensions
   ;   * wmctrl
+  ;     * used to un-maximize
   ;     * provides sizing and movement of active window
+  ;   * xwininfo
+  ;     * provides absolute positioning and measurements of specified window
+  ;     * used to get active window geometry
   ;
   ; optional binaries:
   ;   * xdpyinfo (not used)
   ;     * provides virtual screen dimensions
   ;   * xdotool (not used)
-  ;     * provides sizing and movement of active window
-  ;   *xwininfo (not used)
-  ;     * provides absolute positioning and measurements of specified window
-
-  (restore-active-window!)
-  (xprop-root!)
-  (parse-wmctrl-window-dimensions-line "0x01400b81  1 74   127  577  717  dev Terminal - james@dev: ~")
-  (get-active-window-dimensions)
   )
